e13b1af0f2ffbdefb2d833bf08d863c9f6ee1d4b
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-05-22 15:44:05 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,8 @@
+a=5
+print(type(a))
+b= True
+print(type(b))
+c=0.999
+print(type(c))
+d= "Navya"
+print(type(d))
---
+++ TypeCasting.py
@@ -0,0 +1,17 @@
+n="Navya"
+a= 13
+h= 4.9
+I=True
+print("n:",n)
+print(type(n))
+print("a:",a)
+print(type(a))
+print("h:",h)
+print(type(h))
+print("I:",I)
+print(type(I))
+
+print("\n after typecasting")
+h=int(h)
+print(h)
+print(type(h))
