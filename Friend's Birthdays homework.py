c9e96083be6db5e4e10a19204cd914cecc5a7632
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-05-22 15:55:25 +0530

    Add files via upload

---
+++ Friend's Birthdays homework.py
@@ -0,0 +1,24 @@
+Friend_1 = "Alice"
+Alice_b_day = "23 Jan 2011" 
+Friend_2 = "Ricky"
+Ricky_b_day = "12 Feb 2011"
+Friend_3 = "John"
+John_b_day = "15 Mar 2011"
+Friend_4 = "Sam"
+Sam_b_day = "20 Apr 2011"
+Friend_5 = "Ben"
+Ben_b_day = "25 May 2011"
+
+print(Friend_1,"'s birthday is on", Alice_b_day,"\n")
+
+
+print(Friend_2, "'s birthday is on", Ricky_b_day,"\n")
+
+
+print(Friend_3, "'s birthday is on", John_b_day,"\n")
+
+
+print(Friend_4,"'s birthday is on", Sam_b_day,"\n")
+
+
+print(Friend_5,"'s birthday is on", Ben_b_day,"\n")
