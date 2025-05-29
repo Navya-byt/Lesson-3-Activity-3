63cfbb1db7b818c5f7058149a9aef6447cea62ad
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-05-29 15:39:03 +0530

    Add files via upload

---
+++ Percentage.py
@@ -0,0 +1,16 @@
+Maths = int(input("Enter the Math marks"))
+English = 60
+Science = 70
+Hindi = 50
+Social = 80
+
+sum = Maths + English + Science + Hindi + Social
+
+print(sum)
+
+per = (sum/500) * 100
+
+print (per)
+
+
+
