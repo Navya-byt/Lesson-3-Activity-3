00a7d00d5be7d5fd8305137092e69ff37098b133
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-05-29 15:38:39 +0530

    Add files via upload

---
+++ Count The Notes.py
@@ -0,0 +1,8 @@
+amount = int(input("Enter the amount to withdraw"))
+note_1 = amount// 100
+note_2 = (amount/100)//50
+note_3 = ((amount/100)/50)//10
+
+print (note_1)
+print (note_2)
+print (note_3)
