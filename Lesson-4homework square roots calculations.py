39e0c5da6fcaee62fff2c73839a755d4412e7a01
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-06-01 18:26:15 +0530

    Add files via upload

---
+++ Lesson-4homework square roots calculations.py
@@ -0,0 +1,10 @@
+#Square root calculator greetings#
+print("Welcome to the Square Root Calculator!")
+#Input from the user#
+import math
+print("Enter the number you want to find the square root of:")
+Num=int(input())
+#Square root calculation#
+result = (math.sqrt(Num))
+#Printing the square root#
+print("The square root of",Num,"is",result)
