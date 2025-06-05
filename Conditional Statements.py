72439692104b4e15e7e01a718e49d076f46c02d5
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-06-05 15:43:29 +0530

    Add files via upload

---
+++ Conditional Statements.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number"))
+if num>0:
+    print(num,"The number the user has entered is positive ")
+ 
+if num<0:
+    print(num,"The number the user has entered is negative") 
