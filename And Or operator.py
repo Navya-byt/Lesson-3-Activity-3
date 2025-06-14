31e83b3f125d8023e0ba50d2185c1cf701b5d9ce
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-06-14 15:51:21 +0530

    Add files via upload

---
+++ And Or operator.py
@@ -0,0 +1,16 @@
+a = 3
+b = 6
+c = 9
+if a > 0 and b > 0 and c > 0:
+    print(a,b,c," are positive numbers")
+else:
+    print(a,b,c," are negative numbers")
+
+a = 3
+b = -1
+c = -4
+if a > 0 or b > 0 or c > 0:
+    print(" Either of the number is greater than 0") 
+else:
+    print("No number is greater than 0")
+
