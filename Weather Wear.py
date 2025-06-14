03b7ac7938d08fb4f59c6be43f6913a909d69ea8
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-06-14 14:50:10 +0530

    Add files via upload

---
+++ Weather Wear.py
@@ -0,0 +1,8 @@
+temp = int(input("Enter the temperature: "))
+if (temp > 28):
+  print("It's hot.")
+  print("so wear something light and loose and soft and breathable and drink plenty of water.")
+else:
+  print("It's cold.")
+  print("so wear something warm and thick and drink hot coco.")
+  
