77e8552e7e18706f7eaf4d29ba76fe521aa3058b
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-06-14 16:55:53 +0530

    Add files via upload

---
+++ Identity Operator.py
@@ -0,0 +1,17 @@
+a = [5,6,7,8]
+b = [5,6,7,8]
+c = a
+b = a
+print(a is c)
+print(a is b)
+print(a is not c)
+print(a is not b)
+
+a = [ 1,2,3,4,5 ]
+b = [ 1,2,3,4,5 ]
+c = a
+print( a in c)
+print( a not in c)
+print( a in b)
+print( a not in b)
+
