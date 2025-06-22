912390a6543806915a08df9ce0db287502a6c998
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-06-22 08:36:45 +0530

    Add files via upload

---
+++ Student Eligibility.py
@@ -0,0 +1,5 @@
+studt_age = int(input("Enter student age:"))
+if studt_age > 10 and studt_age < 20:
+  print("Student is eligible to enroll in class")
+else:
+  print("Student is not eligible to enroll in class")
