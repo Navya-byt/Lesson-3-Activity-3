4a7619c38e545f94961f3c90ecf3b141f936382d
Navya-byt <Pakanatinavyasree.np@gmail.com>
2025-10-10 15:04:01 +0530

    Add files via upload

---
+++ Due Amount.py
@@ -0,0 +1,8 @@
+def Due_Amount():
+    bill = 2.50 # Total Amount of the household items
+    paid = 4 # Total Amount Vishal paid
+    due = paid - bill # calllculating the difference
+    return due # the calculated difference
+
+print("Change due:", Due_Amount()) # printing the result
+
